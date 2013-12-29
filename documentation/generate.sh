93a8baedfe0848ef515d587dc9d5c9d7dbcfb9dc
Matt Venn <matt@mattvenn.net>
2013-12-29 11:39:08 +0000

    code highlighting and code inclusion

--- documentation/generate.sh
+++ documentation/generate.sh
@@ -11,7 +11,16 @@ find . -name '*md' | while read path; do
     echo "processing $path"
     #and use pandoc to generate a pdf
 #    pandoc -s -S --toc $file -o $file.html
-    pandoc -s -S -c $doc/pandoc.css -A $doc/footer.html $file -o $file.html
-    wkhtmltopdf $file.html $file.pdf
+    
+    #substitute code, hardcoded for now 
+    sed -e '/^reaction.py/ {                               
+    r reaction.py
+    d }' < README.md > README.md.tmp
+
+    pandoc -s -S --indented-code-classes='prettyprint linenums' -c $doc/pandoc.css -H $doc/header.html -A $doc/footer.html $file.tmp -o $file.html
+#    wkhtmltopdf $file.html $file.pdf
+
+    #remove the tmp file
+    rm README.md.tmp
     cd $root
 done
